121a09d8010e71494051025b03aed05b3d4ff977
Saad Najmi <saadnajmi2@gmail.com>
2021-02-10 17:09:28 -0600

    Initial Commit

---
+++ macos/FluentUITestViewControllers/TestLabelViewController.swift
@@ -0,0 +1,72 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+import AppKit
+import FluentUI
+
+class TestLabelViewController: NSViewController {
+
+    override func loadView() {
+
+        let labels = TextStyle.allCases.map {textStyle -> NSTextField in
+            return createLabel(withString: string(ForTextStyle: textStyle), forTextStyle: textStyle)
+        }
+
+        let gridView = NSGridView(frame: .zero)
+        gridView.rowSpacing = gridViewRowSpacing
+        gridView.columnSpacing = gridViewColumnSpacing
+        gridView.setContentHuggingPriority(.defaultHigh, for: .vertical)
+
+        gridView.addColumn(with: labels)
+
+        let containerView = NSStackView()
+        containerView.orientation = .vertical
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.edgeInsets = NSEdgeInsets(
+            top: containerViewEdgeInsets,
+            left: containerViewEdgeInsets,
+            bottom: containerViewEdgeInsets,
+            right: containerViewEdgeInsets
+        )
+
+        containerView.addView(gridView, in: .top)
+
+        view = containerView
+    }
+
+    func string(ForTextStyle textStyle: TextStyle) -> String {
+        switch textStyle {
+
+        case .largeTitle:
+            return "Large Title"
+        case .title1:
+            return "Title 1"
+        case .title2:
+            return "Title 2"
+        case .title3:
+            return "Title 3"
+        case .headline:
+            return "Headline"
+        case .body:
+            return "Body"
+        case .callout:
+            return "Callout"
+        case .subheadline:
+            return "Subheadline"
+        case .footnote:
+            return "Footnote"
+        case .caption1:
+            return "Caption 1"
+        case .caption2:
+            return "Caption 2"
+        }
+    }
+}
+
+// MARK: - Constants
+fileprivate let gridViewRowSpacing: CGFloat = 20
+
+fileprivate let gridViewColumnSpacing: CGFloat = 20
+
+fileprivate let containerViewEdgeInsets: CGFloat = 20
